9dae4cf9be74a0b9138e83ffc8f9695dbf24c594
GCharles85 <gcharles345@gmail.com>
2024-08-27 18:13:09 -0400

    Initial commit, Home and Diagnostics pages showing up

---
+++ Models/DiagnosticItem.cs
@@ -0,0 +1,10 @@
+namespace Diagnoses.Models
+{
+    public class DiagnosticItem
+    {
+        public string Title { get; set; }
+        public string Cost { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+}
