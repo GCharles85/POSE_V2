3c7fdeccc6d7018ba0f734463ad710510f2fe369
GCharles85 <gcharles345@gmail.com>
2024-08-27 20:42:34 -0400

    add closing curly brace

--- obj/Debug/net8.0/POSE_V2.AssemblyInfo.cs
+++ obj/Debug/net8.0/POSE_V2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POSE_V2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+174622c677b0a501b3271e63fb806a8b1beddb6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82ef1566d505f512964749c66cc2e4e8ea5908ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("POSE_V2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POSE_V2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
