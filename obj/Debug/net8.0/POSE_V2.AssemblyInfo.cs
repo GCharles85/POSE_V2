3d226c8007e37f653b2aab86308c8a7381dbd602
GCharles85 <gcharles345@gmail.com>
2024-08-28 00:16:26 -0400

    diagnostics page properly update when AI button pressed

--- obj/Debug/net8.0/POSE_V2.AssemblyInfo.cs
+++ obj/Debug/net8.0/POSE_V2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POSE_V2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82ef1566d505f512964749c66cc2e4e8ea5908ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c7fdeccc6d7018ba0f734463ad710510f2fe369")]
 [assembly: System.Reflection.AssemblyProductAttribute("POSE_V2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POSE_V2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- py-model/app.py
+++ py-model/app.py
@@ -1,13 +1,33 @@
-from flask import Flask, request, jsonify
-from pymodel import predict
+from flask import Flask, jsonify, send_from_directory
+from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 
-@app.route('/predict', methods=['POST'])
-def predict_route():
-    input_data = request.json['input']
-    result_json = predict(input_data)
-    return jsonify(result_json)
+CORS(app)  # Optionally configure CORS for all routes
+
+# After request handler to manually add CORS headers if needed
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+    return response
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    # Path to the 'wwwroot/data' directory relative to the parent directory of 'py-model'
+    parent_dir = os.path.dirname(os.path.dirname(__file__))
+    wwwroot_path = os.path.join(parent_dir, 'wwwroot/data')
+    
+    # Specify the JSON file you want to return
+    json_filename = 'updated-diagnostics.json'
+    
+    try:
+        # Return the contents of the JSON file
+        return send_from_directory(wwwroot_path, json_filename)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
