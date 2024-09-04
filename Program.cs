11cc80432a7422d912089a453ebc874695e51dac
GCharles85 <gcharles345@gmail.com>
2024-09-04 13:35:17 -0400

    fix error with Configuration object

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
+
 using POSE_V2;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +9,13 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+builder.Services.AddDbContext<DiagnosticContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+    // Other service registrations
+
+
 
 await builder.Build().RunAsync();
